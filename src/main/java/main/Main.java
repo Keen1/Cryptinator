73eb5565c1a4fec547277ccd1d0a0c5e09a1858a
zach <ke3n1@proton.me>
2024-07-13 00:16:41 -0400

    Starting main argument parser

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,43 @@
+package main;
+import net.sourceforge.argparse4j.*;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+        ArgumentParser parser = getArgParser();
+        Namespace ns = null;
+        try{
+            System.out.println(parser.parseArgs(args));
+
+        }catch(ArgumentParserException e){
+            //System.out.println(e.getMessage());
+            parser.handleError(e);
+            System.exit(1);
+        }
+
+
+    }
+    public static ArgumentParser getArgParser(){
+        ArgumentParser ap = ArgumentParsers.newFor("Encryptinator")
+
+                .build()
+                .description("Encrypt or decrypt files and directories.")
+                .defaultHelp(true);
+        //encrypt decrypt args
+        ap.addArgument("-e", "--encrypt");
+        ap.addArgument("-d", "--decrypt");
+
+        //file args
+        ap.addArgument("-f")
+                .nargs("*")
+                .help("List of files to encrypt.");
+        ap.addArgument("-dirs")
+                .nargs("*")
+                .help("List of directories to encrypt.");
+        return ap;
+    }
+}
